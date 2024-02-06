5df740324de4c6399ff6fe5218d9dabbcea79e25
Richard Williams <1234richardwilliams@gmail.com>
2024-02-06 10:23:30 +0000

    Library notebook

---
+++ lib.r
@@ -0,0 +1,16 @@
+# Initial setup scripts
+
+# Load libraries
+suppressMessages({
+  library(sparklyr) 
+  library(dplyr)
+})
+
+# Make helper method "read_datalake" to load csv files
+sc <- spark_connect(method = 'databricks')
+opts <- list(multiline = TRUE)
+read_datalake <- function(filename, folder)
+  spark_read_csv(sc, path = file.path('dbfs:/mnt/raw-data/', folder, filename), opt = opts) %>% collect
+
+message('Loaded libs: sparklyr, dplyr')
+message('Functions available: read_datalake')
