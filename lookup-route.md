4b1698e867bb2970f658e26890cd7465effae0f0
Richard Williams <1234richardwilliams@gmail.com>
2024-02-06 10:21:19 +0000

    Route lookup

---
+++ lookup-route.md
@@ -0,0 +1,15 @@
+%md
+
+# Route lookup
+
+Creates a dataframe `RouteLookup` with columns:
+
+- **RouteDescription** - route description from data
+- **RouteCategory** - oral/injection/sublingual/topical/rectal
+
+## Example output
+
+| RouteDescription       | RouteCategory |
+| ---------------------- | ------------- |
+| IV Continuous Infusion | injection     |
+| Enteral Feeding Tube   | oral          |
---
+++ lookup-route.r
@@ -0,0 +1,32 @@
+# create the route lookup
+RouteLookup <- data.frame()
+ 
+# Build up the lookup. Columns are:
+# - RouteInstruction
+# - RouteCategory (oral/injection/sublingual/topical/rectal)
+
+# Injection
+rbind(RouteLookup,c("EPIDURAL Infusion.","injection")) -> RouteLookup
+rbind(RouteLookup,c("EPIDURAL Injection.","injection")) -> RouteLookup
+rbind(RouteLookup,c("IM","injection")) -> RouteLookup
+rbind(RouteLookup,c("INTRATHECAL  Injection","injection")) -> RouteLookup
+rbind(RouteLookup,c("IV Continuous Infusion","injection")) -> RouteLookup
+rbind(RouteLookup,c("IV Infusion","injection")) -> RouteLookup
+rbind(RouteLookup,c("IV Slow Injection","injection")) -> RouteLookup
+rbind(RouteLookup,c("Intrathecal","injection")) -> RouteLookup
+rbind(RouteLookup,c("SC","injection")) -> RouteLookup
+rbind(RouteLookup,c("SC Continuous Infusion","injection")) -> RouteLookup
+
+# Oral
+rbind(RouteLookup,c("Enteral Feeding Tube","oral")) -> RouteLookup
+rbind(RouteLookup,c("Oral","oral")) -> RouteLookup
+rbind(RouteLookup,c("Oral/IM","oral")) -> RouteLookup
+
+# Sublingual
+rbind(RouteLookup,c("Sublingual","sublingual")) -> RouteLookup
+
+# Topical
+rbind(RouteLookup,c("Topical","topical")) -> RouteLookup
+
+colnames(RouteLookup) <- c("RouteDescription","RouteCategory")
+RouteLookup$RouteCategory <- as.factor(RouteLookup$RouteCategory)
