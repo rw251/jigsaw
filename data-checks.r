2bc49eac40291938ab9c0dd0cb05c522a2c94bf5
Richard Williams <1234richardwilliams@gmail.com>
2024-02-08 12:15:25 +0000

    update

--- data-checks.r
+++ data-checks.r
@@ -13,3 +13,19 @@ if(nrow(Levobupivacaine) == nrow(LevobupivacaineEpidural)) {
   message('  Some Levobupivacaine administrations do not have a route of epidural and so should be included elsewhere.')
   message('\n!!!!CHECK FAILED!!!!\n')
 }
+
+message('# Check the total number of rows in the sub data frames is equal to those in the original data.\n')
+message (paste("  There are", nrow(FilteredData), "rows"))
+message (paste("   -", nrow(SingleDoseRows ), "rows that are a single dose"))
+message (paste("   -", nrow(Injection24HoursRows ), "rows for injections with mg or mcg / 24 hours"))
+message (paste("   -", nrow(InjectionRows ), "rows for injections with ml/hour"))
+message (paste("   -", nrow(PatchRows ), "rows for patches"))
+
+if(nrow(FilteredData) == nrow(SingleDoseRows ) + nrow(Injection24HoursRows) + nrow(InjectionRows) + nrow(PatchRows)) {
+  message('  The rows of the sub-data-frames = the original data frame.')
+  message('\nCHECK PASSED\n')
+} else {
+  message('  The number of sub rows does not equal the original data frame.')
+  message('\n!!!!CHECK FAILED!!!!\n')
+}
+
--- main.r
+++ main.r
@@ -2,10 +2,6 @@
 
 ### SPLIT HERE ###
 
-%run ./data-checks
-
-### SPLIT HERE ###
-
 %run ./process-single-dose
 
 ### SPLIT HERE ###
@@ -22,13 +18,11 @@
 
 ### SPLIT HERE ###
 
-# Check
-message (paste("There are", nrow(FilteredData), "rows"))
-message (paste(" -", nrow(SingleDoseRows ), "rows that are a single dose"))
-message (paste(" -", nrow(Injection24HoursRows ), "rows for injections with mg or mcg / 24 hours"))
-message (paste(" -", nrow(InjectionRows ), "rows for injections with ml/hour"))
-message (paste(" -", nrow(PatchRows ), "rows for patches"))
-
-message(paste0('Check that the number of rows (', nrow(FilteredData), ') is equal to the sum of the components (', nrow(SingleDoseRows ) + nrow(Injection24HoursRows) + nrow(InjectionRows) + nrow(PatchRows),')'))
+%run ./data-checks
 
 ### SPLIT HERE ###
+
+Data <- rbind(SingleDoseMME, PatchMME, InjectionMME, Injection24HoursMME) %>% 
+  group_by(PseudonymisedID, OpioidName, Date) %>%
+  summarise(DailyDose = sum(DailyDose), DailyMME = sum(DailyMME), .groups="drop") %>%
+  filter(DailyMME > 0)
