a89f46f6c0290f1977c7aba71f9053afc956942e
Richard Williams <1234richardwilliams@gmail.com>
2024-02-08 08:10:31 +0000

    Main

--- main.r
+++ main.r
@@ -2,51 +2,33 @@
 
 ### SPLIT HERE ###
 
-### SPLIT HERE ###
-
-
-### SPLIT HERE ###
-
+%run ./data-checks
 
 ### SPLIT HERE ###
 
-# Dosage
-# Focus on tablets. Find all possible combinations to get the quantity  of each dose.
-Tablets <- FilteredData %>% filter(Form == 'tablet')
-
-Tablet_DosHigh <- Tablets %>% filter(!is.na(DosageHigh))
-Tablet_DosHighNA <- Tablets %>% filter(is.na(DosageHigh))
-
-Tablet_DosHighNA_DosLowEQTaskDose <- Tablet_DosHighNA %>% filter(DosageLow == TaskDose)
-Tablet_DosHighNA_DosLowNEQTaskDose <- Tablet_DosHighNA %>% filter(DosageLow != TaskDose)
-Tablet_DosHighNA_TaskDoseNA <- Tablet_DosHighNA %>% filter(is.na(TaskDose))
-Tablet_DosHighAndLow_DosLowEQTaskDose <- Tablet_DosHigh %>% filter(DosageLow == TaskDose)
-Tablet_DosHighAndLow_DosHighEQTaskDose <- Tablet_DosHigh %>% filter(DosageHigh == TaskDose)
-Tablet_DosHighAndLow_NeitherEQTaskDose <- Tablet_DosHigh %>% filter(DosageLow != TaskDose & DosageHigh != TaskDose)
-Tablet_DosHighAndLow_TaskDoseNA <- Tablet_DosHigh %>% filter(is.na(TaskDose))
-
-cat(paste("There are ",nrow(Tablets)," tablet rows.\n"))
-cat(paste("> There are ",nrow(Tablet_DosHighNA)," tablet rows without a DosageHigh.\n"))
-cat(paste(">> ",nrow(Tablet_DosHighNA_TaskDoseNA)," with TaskDose==NA rows.\n"))
-cat(paste(">> ",nrow(Tablet_DosHighNA_DosLowEQTaskDose)," with DosageLow==TaskDose rows.\n"))
-cat(paste(">> ",nrow(Tablet_DosHighNA_DosLowNEQTaskDose)," with DosageLow!=TaskDose rows.\n\n"))
-
-cat(paste("> There are ",nrow(Tablet_DosHigh)," tablet rows with a DosageHigh.\n"))
-cat(paste(">> ",nrow(Tablet_DosHighAndLow_TaskDoseNA)," with TaskDose==NA rows.\n"))
-cat(paste(">> ",nrow(Tablet_DosHighAndLow_DosLowEQTaskDose)," with DosageLow==TaskDose rows.\n"))
-cat(paste(">> ",nrow(Tablet_DosHighAndLow_DosHighEQTaskDose)," with DosageHigh==TaskDose rows.\n"))
-cat(paste(">> ",nrow(Tablet_DosHighAndLow_NeitherEQTaskDose)," with Neither==TaskDose rows.\n\n"))
-
-cat(paste("CHECK: Total number of tablets ", nrow(Tablets), " should equal sum of all components: ", nrow(Tablet_DosHighNA_DosLowEQTaskDose) + nrow(Tablet_DosHighNA_DosLowNEQTaskDose) + nrow(Tablet_DosHighNA_TaskDoseNA) + nrow(Tablet_DosHighAndLow_DosLowEQTaskDose) + nrow(Tablet_DosHighAndLow_DosHighEQTaskDose) + nrow(Tablet_DosHighAndLow_NeitherEQTaskDose) + nrow(Tablet_DosHighAndLow_TaskDoseNA)))
+%run ./process-single-dose
 
 ### SPLIT HERE ###
 
-### SPLIT HERE ###
+%run ./process-patches
 
 ### SPLIT HERE ###
 
-### SPLIT HERE ###
+%run ./process-injections
 
 ### SPLIT HERE ###
+
+%run ./process-injections-24hours
+
+### SPLIT HERE ###
+
+# Check
+message (paste("There are", nrow(FilteredData), "rows"))
+message (paste(" -", nrow(SingleDoseRows ), "rows that are a single dose"))
+message (paste(" -", nrow(Injection24HoursRows ), "rows for injections with mg or mcg / 24 hours"))
+message (paste(" -", nrow(InjectionRows ), "rows for injections with ml/hour"))
+message (paste(" -", nrow(PatchRows ), "rows for patches"))
+
+message(paste0('Check that the number of rows (', nrow(FilteredData), ') is equal to the sum of the components (', nrow(SingleDoseRows ) + nrow(Injection24HoursRows) + nrow(InjectionRows) + nrow(PatchRows),')'))
 
 ### SPLIT HERE ###
