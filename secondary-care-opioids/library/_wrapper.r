1fc10c7806e723b8a44de1678df40fd1cc3bc263
Richard Williams <1234richardwilliams@gmail.com>
2024-02-14 15:48:53 +0000

    Added wrapper script

---
+++ secondary-care-opioids/library/_wrapper.r
@@ -0,0 +1,44 @@
+%run ./data-transformation
+
+### SPLIT HERE ###
+
+%run ./process-single-dose
+
+### SPLIT HERE ###
+
+%run ./process-patches
+
+### SPLIT HERE ###
+
+%run ./process-injections
+
+### SPLIT HERE ###
+
+%run ./process-injections-24hours
+
+### SPLIT HERE ###
+
+%run ./process-injections-pca
+
+### SPLIT HERE ###
+
+%run ./data-checks
+
+### SPLIT HERE ###
+
+# Group the non-PCA data together
+NonPCAData <- rbind(SingleDoseMME, PatchMME, InjectionMME, Injection24HoursMME) %>% 
+  group_by(PseudonymisedID, OpioidName, Date) %>%
+  summarise(DailyDose = sum(DailyDose), DailyMME = sum(DailyMME), .groups="drop") %>%
+  filter(DailyMME > 0 & Date >= '2010-01-01' & Date <= '2021-09-30')
+NonPCAData$IsPCA = FALSE
+
+# Group the PCA data together
+PCAData <- PCAInjectionMME %>% 
+  group_by(PseudonymisedID, OpioidName, Date) %>%
+  summarise(DailyDose = sum(DailyDose), DailyMME = sum(DailyMME), .groups="drop") %>%
+  filter(DailyMME > 0 & Date >= '2010-01-01' & Date <= '2021-09-30')
+PCAData$IsPCA = TRUE
+
+# Combine to final data set
+Data <- rbind(PCAData, NonPCAData)
