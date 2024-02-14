e787dd956b1c0d7498cf406048c297a82d3972fe
Richard Williams <1234richardwilliams@gmail.com>
2024-02-14 16:45:29 +0000

    update

--- secondary-care-opioids/_tests.r
+++ secondary-care-opioids/_tests.r
@@ -10,6 +10,22 @@ Opioid_Medication_Task <- data.frame()
 Opioid_Medication_Task <- rbind(Opioid_Medication_Task,c('1','codeine (15mg/5ml) linctus sugar free','Oral','TaskStatusCode',NA,'Three Times a Day','2021-01-01 15:00:00','',15,15,15,'mg',NA))
 Opioid_Medication_Task <- rbind(Opioid_Medication_Task,c('1','fentanyl patch','Topical','TaskStatusCode',NA,'Daily (6pm)','2021-01-11 18:00:00','Fentanyl;TaskNameis10microgramhr',10,10,10,'microgram/hr',NA))
 
+# Test 2 - Patient 2 has one two doses of oral codeine on the same day
+Opioid_Medication_Task <- rbind(Opioid_Medication_Task,c('2','codeine tablets','Oral','TaskStatusCode',NA,'As Often as Necessary','2021-01-01 12:00:00','',15,15,15,'mg',NA))
+Opioid_Medication_Task <- rbind(Opioid_Medication_Task,c('2','codeine tablets','Oral','TaskStatusCode',NA,'As Often as Necessary','2021-01-01 15:00:00','',15,15,15,'mg',NA))
+
+# Test 3 - Patient 3 has one two doses of oral morphine, and morphine injection crossing midnight
+Opioid_Medication_Task <- rbind(Opioid_Medication_Task,c('3','morphine m/r tablets [mst]','Oral','TaskStatusCode',NA,'As Often as Necessary','2021-01-01 12:00:00','',10,10,10,'mg',NA))
+Opioid_Medication_Task <- rbind(Opioid_Medication_Task,c('3','morphine m/r tablets [mst]','Oral','TaskStatusCode',NA,'As Often as Necessary','2021-01-01 15:00:00','',10,10,10,'mg',NA))
+Opioid_Medication_Task <- rbind(Opioid_Medication_Task,c('3','morphine (30mg/30ml) infusion','IV Infusion','TaskStatusCode',NA,'Every 6 Hours','2021-01-01 20:00:00','',5,5,5,'ml/hour',NA))
+
+# Test 4 - Patient with a 24 hour injection
+Opioid_Medication_Task <- rbind(Opioid_Medication_Task,c('4','oxycodone injection','SC Continuous Infusion','Performed',NA,'Morning','2021-01-01 19:00:00','',15,15,NA,'mg/24hours',NA))
+
+# Test 5 - Patient with 2 PCAs, one of which spans midnight
+Opioid_Medication_Task <- rbind(Opioid_Medication_Task,c('5','fentanyl (20micrograms/ml) pca','IV Infusion','Performed',NA,'Every 6 Hours','2021-01-01 15:00:00','Fentanyl20microgramsmlPCABolus20microgram;LOCKOUT5minutesIVInfusionCONTROLLEDDRUG-LOADINGDOSEUpto200microgramshourly-titratedbyapprovedstaffasperSRFTprotocol',NA,20,NA,'microgram',NA))
+Opioid_Medication_Task <- rbind(Opioid_Medication_Task,c('5','fentanyl (20micrograms/ml) pca','IV Infusion','Performed',NA,'Every 6 Hours','2021-01-01 21:00:00','Fentanyl20microgramsmlPCABolus20microgram;LOCKOUT5minutesIVInfusionCONTROLLEDDRUG-LOADINGDOSEUpto200microgramshourly-titratedbyapprovedstaffasperSRFTprotocol',NA,20,NA,'microgram',NA))
+
 colnames(Opioid_Medication_Task) <- c("PseudonymisedID","MedicationName","TaskRouteCode", "TaskStatusCode", "OrderRouteCode","FrequencyCode","PerformedFromDtm","TaskName","TaskDose","DosageLow","DosageHigh","TaskUom","Uom")
 
 Opioid_Medication_Task$PerformedFromDtm <- as.POSIXct(Opioid_Medication_Task$PerformedFromDtm)
@@ -24,9 +40,9 @@ Opioid_Medication_Task$DosageHigh <- as.numeric(Opioid_Medication_Task$DosageHig
 ### SPLIT HERE ###
 
 # Checks
-isPassing <- TRUE
 
 # Test 1
+isPassing <- TRUE
 message('# Test 1\n')
 Patient001 <- Data %>% filter(PseudonymisedID=='1')
 if(nrow(Patient001) != 3) {
@@ -47,4 +63,80 @@ if(nrow(Patient001 %>% filter(OpioidName == 'fentanyl' & Date == '2021-01-12' & 
 }
 if(isPassing) {
   message('  ✓ Test 1 passed')
+}
+
+# Test 2
+isPassing <- TRUE
+message('\n# Test 2\n')
+Patient002 <- Data %>% filter(PseudonymisedID=='2')
+if(nrow(Patient002) != 1) {
+  message(paste0('  Expecting 1 row for patient 2, but instead got ', nrow(Patient002)))
+  isPassing = FALSE
+}
+if(nrow(Patient002 %>% filter(OpioidName == 'codeine' & Date == '2021-01-01' & DailyDose == 30 & DailyMME == 4.5)) != 1) {
+  message('  Expected row not found in test 2.1')
+  isPassing = FALSE
+}
+if(isPassing) {
+  message('  ✓ Test 2 passed')
+}
+
+# Test 3
+isPassing <- TRUE
+message('\n# Test 3\n')
+Patient003 <- Data %>% filter(PseudonymisedID=='3')
+if(nrow(Patient003) != 2) {
+  message(paste0('  Expecting 2 rows for patient 3, but instead got ', nrow(Patient003)))
+  isPassing = FALSE
+}
+if(nrow(Patient003 %>% filter(OpioidName == 'morphine' & Date == '2021-01-01' & DailyDose == 40 & DailyMME == 80)) != 1) {
+  message('  Expected row not found in test 3.1')
+  isPassing = FALSE
+}
+if(nrow(Patient003 %>% filter(OpioidName == 'morphine' & Date == '2021-01-02' & DailyDose == 10 & DailyMME == 30)) != 1) {
+  message('  Expected row not found in test 3.2')
+  isPassing = FALSE
+}
+if(isPassing) {
+  message('  ✓ Test 3 passed')
+}
+
+# Test 4
+isPassing <- TRUE
+message('\n# Test 4\n')
+Patient004 <- Data %>% filter(PseudonymisedID=='4')
+if(nrow(Patient004) != 2) {
+  message(paste0('  Expecting 2 rows for patient 4, but instead got ', nrow(Patient004)))
+  isPassing = FALSE
+}
+if(nrow(Patient004 %>% filter(OpioidName == 'oxycodone' & Date == '2021-01-01' & DailyDose == 5*15/24 & DailyMME == 3*DailyDose)) != 1) {
+  message('  Expected row not found in test 4.1')
+  isPassing = FALSE
+}
+if(nrow(Patient004 %>% filter(OpioidName == 'oxycodone' & Date == '2021-01-02' & DailyDose == 19*15/24 & DailyMME == 3*DailyDose)) != 1) {
+  message('  Expected row not found in test 4.2')
+  isPassing = FALSE
+}
+if(isPassing) {
+  message('  ✓ Test 4 passed')
+}
+
+# Test 5
+isPassing <- TRUE
+message('\n# Test 5\n')
+Patient005 <- Data %>% filter(PseudonymisedID=='5')
+if(nrow(Patient005) != 2) {
+  message(paste0('  Expecting 2 rows for patient 5, but instead got ', nrow(Patient005)))
+  isPassing = FALSE
+}
+if(nrow(Patient005 %>% filter(OpioidName == 'fentanyl' & Date == '2021-01-01' & abs(DailyDose - 1.8) < 0.0001 & abs(DailyMME - 130*DailyDose) < 0.0001)) != 1) {
+  message('  Expected row not found in test 5.1')
+  isPassing = FALSE
+}
+if(nrow(Patient005 %>% filter(OpioidName == 'fentanyl' & Date == '2021-01-02' & abs(DailyDose - 0.6) < 0.0001 & abs(DailyMME - 130*DailyDose) < 0.0001)) != 1) {
+  message('  Expected row not found in test 5.2')
+  isPassing = FALSE
+}
+if(isPassing) {
+  message('  ✓ Test 5 passed')
 }
