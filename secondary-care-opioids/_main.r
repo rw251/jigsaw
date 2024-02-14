230b5d5c828f889b2aaf2596820e845a4ba94f4b
Richard Williams <1234richardwilliams@gmail.com>
2024-02-14 15:52:43 +0000

    change

--- secondary-care-opioids/_main.r
+++ secondary-care-opioids/_main.r
@@ -4,3 +4,8 @@
 
 # Write to file
 write_to_file(Data, 'secondary-care-opioids-tidied.csv', 'analysis-outputs')
+
+### SPLIT HERE ###
+
+# Read top 10 lines to check it looks ok
+readLines(file.path('/dbfs/mnt/', 'analysis-outputs', 'secondary-care-opioids-tidied.csv'), n=20)
