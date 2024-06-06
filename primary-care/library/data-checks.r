adcdb0c205e0ef468dadd051f1466c6a852b0164
Richard Williams <1234richardwilliams@gmail.com>
2024-06-06 09:52:33 +0100

    More data checks

--- primary-care/library/data-checks.r
+++ primary-care/library/data-checks.r
@@ -59,6 +59,54 @@ if(nrow(o42RecordsWithout10mgOralSol) == 0) {
   error_count <- error_count + 1
 }
 
+# Check 5
+message('# Check that ReadCode dj3.. is always 15mcg/hour Buprenorphine patch.\n')
+dj3Records <- JournalData %>% filter(ReadCode == 'dj3..')
+message(paste0('  There are ', nrow(dj3Records), ' rows with ReadCode=="dj3.."'))
+dj3RecordsWithout15mcgBuprenorphinePatch <- dj3Records %>% filter(
+  Rubric != "Buprenorphine 15micrograms/hour transdermal patches" &
+  Rubric != "Butec 15micrograms/hour transdermal patches (Qdem Pharmac..." &
+  Rubric != "Butec 15micrograms/hour transdermal patches (Qdem Pharmaceuticals Ltd)" &
+  Rubric != "BuTrans 15micrograms/hour transdermal patches (Napp Pharm..." &
+  Rubric != "BuTrans 15micrograms/hour transdermal patches (Napp Pharmace"
+)
+if(nrow(dj3RecordsWithout15mcgBuprenorphinePatch) == 0) {
+  message('  ✓ All rows with a ReadCode dj3.. have a rubric suggesting 15mcg/hour Buprenorphine patch')
+  message('  ✓ CHECK PASSED\n')
+} else {
+  message('  Some rows with a ReadCode dj3.. do not have a rubric suggesting 15mcg/hour Buprenorphine patch.')
+  message('\n!!!!CHECK FAILED!!!!\n')
+  error_count <- error_count + 1
+}
+
+# Check 6
+message('# Check that ReadCode djks. is always one of the listed Rubrics.\n')
+djksRecords <- JournalData %>% filter(ReadCode == 'djks.')
+message(paste0('  There are ', nrow(djksRecords), ' rows with ReadCode=="djks."'))
+djksRecordsWithoutKnownRubric <- djksRecords %>% filter(
+  Rubric != "OxyNorm liquid 5mg/5ml oral solution (Napp Pharmaceutical..." &
+  Rubric != "Oxycodone 5mg/5ml oral solution sugar free" &
+  Rubric != "OXYNORM liq 5mg/5ml" &
+  Rubric != "OXYCODONE HCl oral liq 5mg/5ml" &
+  Rubric != "OxyNorm liquid 1mg/ml oral solution (Napp Pharmaceuticals..." &
+  Rubric != "OXYCODONE HCl sf oral soln 5mg/5ml" &
+  Rubric != "OXYNORM oral soln 5mg/5ml" &
+  Rubric != "Oxycodone Hydrochloride  Solution  Sugar Free 5 mg/5 ml" &
+  Rubric != "OxyNorm liquid 1mg/ml oral solution (Napp Pharmaceuticals Ltd)" &
+  Rubric != "OxyNorm  Oral Solution  5 mg/5 ml" &
+  Rubric != "Oxycodone Hydrochloride Solution Sugar Free 5 mg/5 ml" &
+  Rubric != "Oxycodone 10mg/ml oral solution sugar free" &
+  Rubric != "OxyNorm 10mg/ml concentrate oral solution (Napp Pharmaceu..." &
+  Rubric != "OXYCODONE HCl oral liq conc 10mg/ml"
+)
+if(nrow(djksRecordsWithoutKnownRubric) == 0) {
+  message('  ✓ All rows with a ReadCode djks. have a known Rubric')
+  message('  ✓ CHECK PASSED\n')
+} else {
+  message('  Some rows with a ReadCode djks. do not have a known rubric.')
+  message('\n!!!!CHECK FAILED!!!!\n')
+  error_count <- error_count + 1
+}
 
 # FINAL
 if(error_count > 0) {
