d47942d9383f04574ae2aab89c886d13e89243f0
Richard Williams <1234richardwilliams@gmail.com>
2024-06-11 17:11:58 +0100

    -Updated readme -added exhaust dose

--- primary-care/library/_wrapper.r
+++ primary-care/library/_wrapper.r
@@ -40,12 +40,12 @@
 
 # Group the data together
 Data <- rbind(TabletMME, PatchMME, OralSolutionMME, SachetMME) %>% 
-  group_by(PseudonymisedID, OpioidName, Date) %>%
-  summarise(Dose = sum(Dose), MME = sum(MME), .groups="drop") %>%
+  group_by(PseudonymisedID, OpioidName, Date, Category) %>%
+  summarise(Dose = sum(Dose), MME = sum(MME), ExhaustDose = sum(ExhaustDose), ExhaustMME = sum(ExhaustMME), .groups="drop") %>%
   filter(MME > 0 & Date >= '2010-01-01' & Date <= '2021-09-30')
 
 # Make Dose column explicitly in milligrams, relocate columns and sort
 Data <- Data %>%
-  rename(DoseInMg = Dose) %>%
+  rename(DoseInMg = Dose, DoseToExhaustInMg = ExhaustDose) %>%
   relocate(Date, .before = OpioidName) %>%
   arrange(PseudonymisedID, Date, OpioidName)
